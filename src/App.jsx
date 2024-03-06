5e66801f286ae14d1c07213bdf320e2bfb785f43
hamru <goo.hiro.w@gmail.com>
2024-03-06 21:18:14 +0900

    fix: mock

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import './styles.css'
 
 export const App = () => {
+    const [incompleteItems, setIncompleteItems] = useState(["モダンな技術の学習", "ソフトウェアテスト工学"]);
+    const [completeItems, setCompleteItems] = useState(["QAエンジニアの役割", "体重測定"]);
     return (
         <>
             <div className='input-area'>
@@ -10,30 +13,28 @@ export const App = () => {
             <div className='incomplete-area'>
                 <p className='title'>試したいこと</p>
                 <ul>
-                    <li>
-                        <div className='list-row'>
-                            <p className='try-item'>モダンな技術の習得</p>
-                            <button>完了</button>
-                            <button>削除</button>
-                        </div>
-                    </li>
+                    {incompleteItems.map((item) => (
+                            <li key={item}>
+                                <div className='list-row'>
+                                    <p className='try-item'>{item}</p>
+                                    <button>完了</button>
+                                    <button>削除</button>
+                                </div>
+                            </li>
+                    ))}
                 </ul>
             </div>
             <div className='complete-area'>
                 <p className='title'>試したこと（新しい体験・学んだこと）</p>
                 <ul>
-                    <li>
-                        <div className='list-row'>
-                            <p className='try-item'>ソフトウェアテスト工学</p>
-                            <button>戻す</button>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='list-row'>
-                            <p className='try-item'>QAエンジニアの役割</p>
-                            <button>戻す</button>
-                        </div>
-                    </li>
+                    {completeItems.map((item) => (
+                        <li key={item}>
+                            <div className='list-row'>
+                                <p className='try-item'>{item}</p>
+                                <button>戻す</button>
+                            </div>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </>
